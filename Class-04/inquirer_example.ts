259fcbf8e4c260cf17a509b6c84fc9000cd143e2
itsAhmedCoder <54277079+itsAhmedCoder@users.noreply.github.com>
2023-11-27 11:56:49 +0500

    Update inquirer_example.ts

--- Class-04/inquirer_example.ts
+++ Class-04/inquirer_example.ts
@@ -1,1 +1,24 @@
+// Author: Ahmed
+// Group: B53Q1 Group-Ahmed
+// Description: Inquirer Example
 
+// Import Inquirer Package
+import inquirer from "inquirer";
+
+// Get Input from the User
+let userinput = await inquirer.prompt([
+    {
+        name: "name",
+        type: "string",
+        message: "Enter name:"
+    },
+    {
+        name: "rollno",
+        type: "number",
+        message: "Enter roll no:"
+    }
+]);
+
+// Display user inputs
+console.log(`Your Name: ${userinput.name}`);
+console.log(`Your Roll No: ${userinput.rollno}`);
